d8e245d739abd7499f24e8c90744060e1e1dcee2
dongjay00 <cjswodlehdgn@gmail.com>
2022-08-05 00:19:29 +0900

    [feat]: 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import LoginPage from "@/pages/loginPage";
+import SignupPage from "@/pages/signupPage";
+import MainPage from "@/pages/mainPage";
 
 function App() {
-  return <div>프리온보딩</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/auth/*" element={<AuthRouter />} />
+    </Routes>
+  );
+}
+
+function AuthRouter() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/auth/login" replace />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+    </Routes>
+  );
 }
 
 export default App;
